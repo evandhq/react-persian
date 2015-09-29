73b054f67b2584b7ec21d5e955f94f890ec6a9b2
Mohamad Mohebifar <mohamad@mohebifar.com>
2015-09-29 19:25:46 +0330

    feat: Component wrapper

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import PersianNumber from './PersianNumber.js';
+import PersianNumberWords from './PersianNumberWords.js';
+
+const components = {
+  PersianNumber,
+  PersianNumberWords
+};
+
+export default components;
