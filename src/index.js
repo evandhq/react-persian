da7ec047976d10560a8c536eb241d432266c24ee
Farzad YZ <farskid@gmail.com>
2017-11-05 19:53:29 +0330

    Adds PersianFileSize to index

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import PersianNumber, { latinToPersian, arabicToPersian, formatString, removeCommasFromString } from './PersianNumber.js';
 import PersianNumberWords from './PersianNumberWords.js';
+import PersianFileSize from './PersianFileSize';
 
 const components = {
   PersianNumber,
   PersianNumberWords,
+  PersianFileSize,
   latinToPersian,
   arabicToPersian,
   formatString,
